6d3485fa71764fa0baede9141e3e1c50f1d6e461
김상은 <81676542+SangAu124@users.noreply.github.com>
2022-03-17 11:24:05 +0900

    2022.03.17_java

---
+++ 20220316/src/kr/hs/dgsw/Ex1/People.java
@@ -0,0 +1,38 @@
+package kr.hs.dgsw.Ex1;
+
+public class People {
+	
+	
+	String name;
+	double height;
+	double weight;
+	double BMI;
+	String Fat;
+	
+	public double calBMI() {
+		
+		double cmHeight = (this.height*0.01);
+		
+		double BMI = (this.weight / (cmHeight * cmHeight));
+		return BMI;
+	}
+	
+	public String isFat() {
+		String Fat;
+		if(this.BMI < 20) {
+			Fat = "저체중";
+		}
+		else if(this.BMI < 25 || this.BMI >= 20) {
+			Fat = "표준체중";
+		}
+		else {
+			Fat = "과체중";
+		}
+		return Fat;
+	}
+	
+	public void printBMI(String name, double BMI, String Fat) {
+		System.out.printf("%s의 비만도는 %.2f이고 %s입니다.\n", name, BMI, Fat);
+	}
+	
+}
---
+++ 20220316/src/kr/hs/dgsw/Ex1/Start.java
@@ -0,0 +1,27 @@
+package kr.hs.dgsw.Ex1;
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		People culsu = new People();
+		People yunghe = new People();
+		
+		culsu.name = "culsu";
+		culsu.height = 170;
+		culsu.weight = 90;
+		culsu.BMI = culsu.calBMI();
+		culsu.Fat = culsu.isFat();
+		
+		yunghe.name = "yunghe";
+		yunghe.height = 165;
+		yunghe.weight = 55;
+		yunghe.BMI = yunghe.calBMI();
+		yunghe.Fat = yunghe.isFat();
+		
+		culsu.printBMI(culsu.name, culsu.BMI, culsu.Fat);
+		yunghe.printBMI(yunghe.name, yunghe.BMI, yunghe.Fat);
+		
+	}
+	
+}
